dab6d83908af57228e60d270e021fcc22ce560dd
Analysis Mshad <nanicku@gmail.com>
2023-02-16 00:28:26 +0300

    run

---
+++ Cinema/src/main/java/edu/school21/cinema/servlets/IndexServlet.java
@@ -0,0 +1,33 @@
+package edu.school21.cinema.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class IndexServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect(req.getContextPath() + "/hello");
+    }
+}
+
+
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@WebServlet("/")
+//public class IndexServlet extends HttpServlet {
+//
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//        //        getServletContext().getRequestDispatcher("/hello").forward(req, resp);
+//        resp.sendRedirect(req.getContextPath() + "/hello");
+//    }
+//}
--- Cinema/src/main/java/edu/school21/cinema/servlets/SignInServlet.java
+++ Cinema/src/main/java/edu/school21/cinema/servlets/SignInServlet.java
@@ -7,7 +7,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class SignInServlet extends HttpServlet{
+//@WebServlet("/signIn")
+
+@WebServlet("/hello")
+public class SignInServlet extends HttpServlet {
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+//
+//    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        HttpSession session = req.getSession();
+        Integer visitCounter = (Integer) session.getAttribute("visitCounter");
+        if (visitCounter == null) {
+            visitCounter = 1;
+        } else {
+            visitCounter++;
+        }
+        session.setAttribute("visitCounter", visitCounter);
+        String username = req.getParameter("username");
+        res.setContentType("text/html");
+        PrintWriter printWriter = res.getWriter();
+        if (username == null) {
+            printWriter.write("Hello, Anonymous" + " ");
+        } else {
+            printWriter.write("Hello, " + username + " ");
+        }
+        printWriter.write("Page was visited " + visitCounter + " times.");
+        printWriter.close();
+    }
 
 }
+
+//        PrintWriter pw = res.getWriter();
+//
+//        pw.println("<html>");
+//        pw.println("<h1> Hello <?h1>");
+//        pw.println("</html");
+//    }
+//}
